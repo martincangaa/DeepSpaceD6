965f9b6a507a71d83abf79985d78ae3802e473f1
IznCd <izn.cd53@gmail.com>
2023-12-14 11:07:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+    
+def user_input_menu():
+    pass
+
+def throw_dice():
+    pass
+
+def get_crew():
+    # call throw_dice
+    pass 
+
+def get_thread():
+    # call throw_dice
+    pass
+
+
+
 def main():
-    x = 0
+    difficulty = menu() #loop --> returns difficulty level, prints high scores
+    while running:
+        print_interface() # prints the ship, health, cards, crew
+        crew = get_crew()
+
+        threads = get_thread()
+
+
+
+
 if __name__ == "__main__":
+    CREW_COMMANDER = 0
+    CREW_TACTICAL = 1
+    CREW_MEDICAL = 2
+    CREW_SCIENCE = 3
+    CREW_ENGINEERING = 4
+    CREW_SCANNER = 5
+    health = 8
+    shield = 4
     main()
