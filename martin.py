c2721de6093f9278ae4e647715650af016a7acc5
martincangaa <91851271+martincangaa@users.noreply.github.com>
2023-12-22 19:58:09 +0100

    Fix typo and update enemy data structure

---
+++ martin.py
@@ -0,0 +1,11 @@
+# COMPLEX FUNCTION,behaviour described in its appearance in the running loop, I think it's better to understand if you see it there
+def assign_crew(crew, active_threats):
+    pass
+
+# throws a dice and returns it's value
+def throw_dice():
+    pass
+
+# Restarts the status of the crewmates that can be gathered back 
+def gather_crew(crew):
+    pass
