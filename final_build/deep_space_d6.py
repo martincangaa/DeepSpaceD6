be49c3e5c7bba15e7380c58815c0fb79e1bab323
martincangaa <91851271+martincangaa@users.noreply.github.com>
2024-01-02 22:36:45 +0100

    Refactor code structure and import statements

--- final_build/deep_space_d6.py
+++ final_build/deep_space_d6.py
@@ -1,7 +1,9 @@
 import os
 import keyboard
 import time
-
+import file_mgmt as fm
+import game_logic as gl
+import input_output_user as io
 
 CREW_COMMANDER = 0
 CREW_TACTICAL = 1
@@ -21,37 +23,37 @@ def main():
     active_threats = []
     n_external_defeated = 0 # the number of enemies defeated
 
-    difficulty = menu()
+    menu()
     
     check_dificulty(difficulty)
 
     while True:
- 
-        print_interface(health, shield, crew, threats, dice_number)
+
+        io.print_interface(health, shield, active_threats, crew)
 
         time.sleep(3) # in order for the user to see the board before loosing or restarting
 
         #Conditions to loose
-        if health <= 0 or can_be_gathered(crew) == False: 
+        if health <= 0 or gl.can_be_gathered(crew) == False: 
             win = False # determine the screen to be printed after ending the loop (see if-statement out of the loop)
             break   # get out of the while
 
-        gather_crew(crew)
+        crew = gl.get_crew(crew)
 
-        get_crew(crew)
-
-        print_interface(health, shield, crew, threats, dice_number)
+        iu.print_interface(health, shield, active_threats, crew)
         user_confirmation() # provisional
 
-        check_scanners(crew)
+        gl.check_scanners(crew)
         
-        print_interface(health, shield, crew, threats, dice_number)
+        io.print_interface(health, shield, active_threats, crew)
 
         # COMPLEX FUNCTION --> probably will start a loop until the user can't perform anymore actions or they decide they dont want to do anything else
         # will check if the active_threats can be solved with any current crewmate, if it is possible to get a crewmate out of the infirmary...
         # all the available options will be shown to the user when they select a crewmate and then they will be able to choose one, whathever they do will probably have consequences
         # (crewmate might me blocked when selecting one of it's possible actions for example)
-        assign_crew(crew, active_threats)
+        
+        crew, active_threats, health, shield = io.assign_crew(crew, active_threats, health, shield)
+
         # |
         # |-> 
             # for every repetition inside the assign_crew we will use at least this:
@@ -65,16 +67,18 @@ def main():
 
         add_threat(threats, active_threats)
 
-        print_interface(health, shield, crew, threats, dice_number)
+        io.print_interface(health, shield, active_threats, crew, user_confirmation=True)
 
-        dice_number = activate_threats(active_threats, crew)
+        gl.activate_threats(active_threats, crew)
+
+        dice_number = activate_threat(active_threats, crew)
 
     # Prints different screens depending on the result of the game
     # In both screens the user will be asked if they want to play again, if the answer is yes, the main method will be called
     if win:
-        win_game()
+        io.win_game()
     else:
-        game_over()
+        io.game_over()
 
 if __name__ == "__main__":
     main()
